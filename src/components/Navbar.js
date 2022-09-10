f07028a4cc0b7cf2bf7aa082692ccad1f3313b59
Shubham <privatestartup2004@gmail.com>
2022-09-10 16:23:31 +0530

    Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
                 <nav className="navbar navbar-dark fixed-top">
                     <div className="nav container-fluid">
                         <img src="https://lh6.googleusercontent.com/-bTUHiGV6SyE/AAAAAAAAAAI/AAAAAAAAAAA/ggDZJFnZHrk/s55-p-k-no-ns-nd/photo.jpg" alt="Logo" />
-                        <a className="navbar-brand fs-1" href="#" ><strong>Wood Worker</strong></a>
+                        <a className="navbar-brand fs-1" href="/" ><strong>Wood Worker</strong></a>
                         <a className="navbar-brand fs-1"><strong>Wood Worker</strong></a>
                         <div className="img"><a href="tel:+91 7498731202"><img src="https://png.pngtree.com/png-vector/20191022/ourmid/pngtree-phone-icon-isolated-on-abstract-background-png-image_1847735.jpg" alt="Call" height={50} width={50} /></a></div>
                     </div >
